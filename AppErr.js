7e9f5566bb52ebef9e81da30189ad1205662c110
Blaine Simpson <blaine.simpson@admc.com>
2022-03-18 13:00:15 -0400

    Correcly export the components now, incl. conciseCatcher

--- AppErr.js
+++ AppErr.js
@@ -2,50 +2,10 @@
 
 const { validate } = require("bycontract-plus");
 
-class AppErr extends Error {
+module.exports = class AppErr extends Error {
     constructor() {
         validate(arguments, [], false);
         super(require("util").format.apply(null, arguments));
         this.name = "AppErr";
     }
 }
-module.exports.AppErr = AppErr;
-
-/**
- * This encapsulates handling of expected exceptions in a way that supports
- * both main thread and async threads.
- *
- * @param exitValue defaults to no-exit-value.  I.e. will never exit.
- *   If set to a positive integer and if function execution throws
- *   anything then after generating appropriate text output, we will
- *   exit the process with the given return value.
- */
-module.exports.conciseCatcher = function(wrappedFn, exitValue) {
-    validate(arguments, ["function", "posint="]);
-    return function() {
-        try {
-            wrappedFn.apply(null, arguments);
-        } catch(e) {
-            if (e === null)
-                console.error("A null was thrown.  "
-                  + "Try using 'node --trace-uncaught' "
-                  + "if you need the stack trace");
-            else if (typeof(e) !== "object")
-                console.error(`A ${typeof(e)} (non-object) was thrown.  `
-                  + "Try using 'node --trace-uncaught' "
-                  + "if you need the stack trace");
-            else if (!("stack" in e))
-                console.error(`An object with no stack was thrown.  `
-                  + "Try using 'node --trace-uncaught' "
-                  + "if you need the stack trace");
-            else if (e.name === "AppErr")
-                console.error("Aborting.  " + e.message);
-            else
-                // Weakness here is that you lose OOTB display of the actual
-                // source line of code.  Since AppErr will be thrown far
-                // more often, this is acceptable.
-                console.error(e.stack);
-            if (exitValue !== undefined) process.exit(exitValue);
-        }
-    };
-};
---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports.AppErr = require("./AppErr");
+module.exports.conciseCatcher = require("./conciseCatcher");
+module.exports.NetRC = require("./NetRC");
