425223ac2a688d0af5d6a3e7c68fa9b1eb526c61
Blaine Simpson <blaine.simpson@admc.com>
2022-03-19 01:06:03 -0400

    Forgot to add JsShell to exports

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ module.exports.AppErr = require("./AppErr");
 const appErrHandlers = require("./appErrHandlers");
 for (let key in appErrHandlers) module.exports[key] = appErrHandlers[key];
 module.exports.NetRC = require("./NetRC");
+module.exports.JsShell = require("./JsShell");
