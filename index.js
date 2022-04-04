38fcb2c3ee37f342a89da1dc53a7c5eb2c328d5c
Blaine Simpson <blaine.simpson@admc.com>
2022-04-04 12:01:54 -0400

    Fixed bug with appVersion reporting version for wrong app.  This is API-breaking.

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const appErrHandlers = require("./appErrHandlers");
 for (let key in appErrHandlers) module.exports[key] = appErrHandlers[key];
 module.exports.NetRC = require("./NetRC");
 module.exports.JsShell = require("./JsShell");
-module.exports.appVersion = JSON.parse(
-  require("fs").readFileSync(require("path").join(__dirname, "package.json"))).
-  version;
+module.exports.getAppVersion = dirName => {
+    if (typeof dirName !== "string") throw new Error(
+      "getAppVersion must be called like: getAppVersion(__dirname)");
+    return JSON.parse(
+      require("fs").readFileSync(require("path").
+      join(dirName, "package.json"))).version;
+};
 module.exports.isPlainObject = val =>
   typeof(val) === "object" && val !== null &&
     Object.getPrototypeOf(val) === Object.getPrototypeOf({});
