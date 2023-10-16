2aaa4a76b7977e57d8c298ee035ada479c85910d
Blaine Simpson <blaine.simpson@admc.com>
2023-10-15 21:34:29 -0400

    Updated unit test for previous commit

--- test/mkDate.js
+++ test/mkDate.js
@@ -4,17 +4,22 @@ const assert = require("node:assert/strict");
 describe("apputils 'mkDate' function", () => {
     it("param not a string", () => {
         assert.throws(() => { mkDate(null); }, TypeError);
-        assert.throws(() => { mkDate(null); }, TypeError);
+        assert.throws(() => { mkDate(123); }, TypeError);
     });
-    it("param not a date", () => {
-        assert.throws(() => { mkDate("bad"); }, TypeError);
+    it("generate a default Date", () => {
+        const newDate = mkDate("2023-04-05T01:02:03.345Z");
+        assert.ok(newDate instanceof Date);
+        assert.equal(1, newDate.getUTCHours());
+        assert.equal(2, newDate.getUTCMinutes());
+        assert.equal(3, newDate.getUTCSeconds());
+        assert.equal(345, newDate.getUTCMilliseconds());
     });
-    it("param not a strictdate", () => {
-        assert.throws(() => { mkDate("20-12-13"); }, TypeError);
-        assert.throws(() => { mkDate("2022-21-22"); }, TypeError);
-        assert.throws(() => { mkDate("2022-12-2"); }, TypeError);
-    });
-    it("generate a Date", () => {
-        assert.ok(mkDate("2023-04-05") instanceof Date);
+    it("generate a time-zeroed Date", () => {
+        const newDate = mkDate("2023-04-05");
+        assert.ok(newDate instanceof Date);
+        assert.equal(0, newDate.getHours());
+        assert.equal(0, newDate.getMinutes());
+        assert.equal(0, newDate.getSeconds());
+        assert.equal(0, newDate.getMilliseconds());
     });
 });
